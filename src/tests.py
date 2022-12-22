b64f1a72eb8e49b81221d73e88cdc7b17f58ca42
tham29 <80027246+tham29@users.noreply.github.com>
2022-12-22 11:31:20 +0700

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -9,4 +9,4 @@ class TestOperationsMethods(unittest.TestCase):
         self.assertEqual("Running app 'app1' in namespace 'dev' nice", self.operations.runningInfo("app1","dev"))
  
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 
